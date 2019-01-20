6fd89ce7363e7a2ff61d049290ddd2bffb250c4e
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-20 15:26:15 +0800

    增加 203. Remove Linked List Elements

---
+++ 203_Remove_Linked_List_Elements/main.go
@@ -0,0 +1,29 @@
+package Remove_Linked_List_Elements
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{Next: head}
+	cur := dummyHead
+
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummyHead.Next
+}
