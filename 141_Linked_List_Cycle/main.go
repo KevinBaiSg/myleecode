d0f76ab7e4b605270fc0cd6dff4055024e117eb6
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-20 23:12:57 +0800

    增加 141. Linked List Cycle

---
+++ 141_Linked_List_Cycle/main.go
@@ -0,0 +1,39 @@
+package Linked_List_Cycle
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/*
+1.遍历链表，记录链表地址，如果发现之前记录过，代表存在环
+2.使用快慢指针的方法。
+*/
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow := head
+	fast := head.Next
+
+	for slow != nil && fast != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		if fast.Next == nil {
+			return false
+		}
+		fast = fast.Next.Next
+	}
+	return false
+}
