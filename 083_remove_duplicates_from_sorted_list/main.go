c783a7b8b8e823cd1d70bba8f4a1fd3990ab6038
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-20 22:43:12 +0800

    增加 83. remove duplicates from sorted list

---
+++ 083_remove_duplicates_from_sorted_list/main.go
@@ -0,0 +1,31 @@
+package remove_duplicates_from_sorted_list
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	cur := head
+	for cur != nil && cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return head
+}
