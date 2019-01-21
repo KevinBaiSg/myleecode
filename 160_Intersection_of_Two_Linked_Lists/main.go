8ef06fb077ab5b4ded290818e4bc34da9348eb5c
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-21 20:00:02 +0800

    增加 160 问题的一种实现方法

--- 160_Intersection_of_Two_Linked_Lists/main.go
+++ 160_Intersection_of_Two_Linked_Lists/main.go
@@ -17,10 +17,16 @@ type ListNode struct {
 /*
 1.暴力方法：for 嵌套
 2.枚举一条链表，记录地址到set中，然后枚举另一条链表，set 中查找是否存在。
-3.把一条短的链表尾部接入另一条的头部，问题转换为求闭环的问题。
+3.两条链表同时移动，当一条到尾部的时候，该节点next接到短链的头，长链继续移动，
+	原理是，同时移动回到原点可以保证两条链表同步了
 4.计算两条链表的长度，求逆差后，在同等长度位置开始枚举链表，查找相同值。
 */
-func getIntersectionNode(headA, headB *ListNode) *ListNode {
+func getIntersectionNode2(headA, headB *ListNode) *ListNode {
+
+	return nil
+}
+
+func getIntersectionNode3(headA, headB *ListNode) *ListNode {
 	if headA == nil || headB == nil {
 		return nil
 	}
@@ -33,46 +39,39 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 			Pc.Next = nil
 		}
 	}()
-
 	for Pa.Next != nil && Pb.Next != nil {
 		Pa = Pa.Next
 		Pb = Pb.Next
 	}
 
-	if Pa.Next == nil {
-		Pa.Next = headB
-		Pc = Pa
+	if Pa.Next == nil && Pb.Next == nil {
 		Pa = headA
-	} else {
-		Pb.Next = headA
-		Pc = Pb
+		Pb = headB
+	} else if Pa.Next == nil {
+		Pc = Pa
 		Pa = headB
+		Pb = Pb.Next
+		Pc.Next = headA
+	} else {
+		Pc = Pb
+		Pb = headA
+		Pa = Pa.Next
+		Pc.Next = headB
 	}
 
-	slow := Pa
-	fast := Pa.Next
-
-	for slow != fast {
-		if slow != nil || fast != nil || fast.Next != nil {
-			return nil
+	for Pa != nil && Pb != nil {
+		if Pa == Pb {
+			return Pa
 		}
-		slow = slow.Next
-		fast = fast.Next.Next
+		Pa = Pa.Next
+		Pb = Pb.Next
 	}
-
-	slow = Pa
-	fast = fast.Next
-	for slow != fast {
-		slow = slow.Next
-		fast = fast.Next
-	}
-
-	return slow
+	return nil
 }
 
-func main() {
-	head := ListNode{Val: 8, Next: &ListNode{4, &ListNode{5, nil}}}
-	headA := ListNode{1, &head}
-	headB := ListNode{1, &head}
-	getIntersectionNode(&headA, &headB)
-}
+//func main() {
+//	head := ListNode{2, &ListNode{4, nil}}
+//	headA := ListNode{0, &ListNode{9, &ListNode{1, &head}}}
+//	headB := ListNode{3, &head}
+//	getIntersectionNode3(&headA, &headB)
+//}
