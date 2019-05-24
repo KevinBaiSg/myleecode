f5157efe7b62ac56e2c0c5c8e5e225b5eaacdb8f
Kevin Bai <kevin.bai.sin@gmail.com>
2019-05-24 23:38:37 +0800

    使用 go 交换变量的方式优化

--- 070_climbing_stairs/climbing_stairs.go
+++ 070_climbing_stairs/climbing_stairs.go
@@ -11,18 +11,12 @@ func climbStairs(n int) int {
 	if n == 0 || n == 1 || n == 2 {
 		return n
 	}
-	mem := make([]int, 3)
-	mem[0] = 1
-	mem[1] = 2
-	/*
-			  mem[0]
 
-		mem[1]		mem[2]
-	*/
+	x, y := 1, 2
 
 	for i := 2; i < n; i++  {
-		mem[i % 3] = mem[(i - 1) % 3] + mem[(i - 2) % 3]
+		x, y = y, x + y
 	}
 
-	return mem[(n - 1) % 3]
+	return y
 }
