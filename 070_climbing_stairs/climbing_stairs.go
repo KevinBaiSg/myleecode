90e8e054ef0642291cc5d4940c21a8e07a5b4f35
Kevin Bai <kevin.bai.sin@gmail.com>
2019-05-24 23:34:57 +0800

    优化了内存使用

--- 070_climbing_stairs/climbing_stairs.go
+++ 070_climbing_stairs/climbing_stairs.go
@@ -11,12 +11,18 @@ func climbStairs(n int) int {
 	if n == 0 || n == 1 || n == 2 {
 		return n
 	}
-	mem := make([]int, n)
+	mem := make([]int, 3)
 	mem[0] = 1
 	mem[1] = 2
+	/*
+			  mem[0]
+
+		mem[1]		mem[2]
+	*/
+
 	for i := 2; i < n; i++  {
-		mem[i] = mem[i - 1] + mem[i - 2]
+		mem[i % 3] = mem[(i - 1) % 3] + mem[(i - 2) % 3]
 	}
 
-	return mem[n - 1]
+	return mem[(n - 1) % 3]
 }
