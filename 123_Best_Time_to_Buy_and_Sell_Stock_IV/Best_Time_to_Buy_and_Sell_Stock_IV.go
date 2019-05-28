85f273ccd18d9f4925e628702bd820e38410839d
Kevin Bai <kevin.bai.sin@gmail.com>
2019-05-28 21:45:16 +0800

    实现问题第一版本，但是存在k过大后内存问题，需要做状态压缩

---
+++ 123_Best_Time_to_Buy_and_Sell_Stock_IV/Best_Time_to_Buy_and_Sell_Stock_IV.go
@@ -0,0 +1,62 @@
+package _123_Best_Time_to_Buy_and_Sell_Stock_III
+
+import "math"
+
+func maxProfit(k int, prices []int) int {
+	if k == 0 { return 0 }
+	if prices == nil || len(prices) == 0 || len(prices) == 1 { return 0 }
+
+	// MP[i][x][j] j=0:没有股票 x:交易次数 j=1:拥有一股
+	MP := make([][][]int, len(prices), len(prices))
+	for i := 0; i < len(prices); i++ {
+		MP[i] = make([][]int, k+1, k+1)
+		for x := 0; x <= k; x++ {
+			MP[i][x] = make([]int, 2, 2)
+		}
+	}
+
+	for x := 0; x <= k ; x++ {
+		if x == 0 {
+			MP[0][x][0] = 0
+			MP[0][x][1] = -prices[0]
+		} else {
+			MP[0][x][0], MP[0][x][1] = math.MinInt32, math.MinInt32
+		}
+	}
+
+	for i := 1; i < len(prices); i++  {
+		for x := 0; x <= k; x++  {
+			if x == 0 {
+				MP[i][0][0] = MP[i-1][0][0]
+				MP[i][0][1] = Max(MP[i-1][0][0] - prices[i], MP[i-1][0][1])
+			} else if x == k {
+				MP[i][x][0] = Max(MP[i-1][x][0], MP[i-1][x-1][1]+prices[i])
+			} else {
+				MP[i][x][0] = Max(MP[i-1][x][0], MP[i-1][x-1][1]+prices[i])
+				MP[i][x][1] = Max(MP[i-1][x][0]-prices[i], MP[i-1][x][1])
+			}
+		}
+	}
+	return MaxRes(MP[len(prices) - 1], k)
+}
+
+func Max(m ...int) int {
+	if len(m) == 0 { return 0 }
+	max := m[0]
+	for _, v := range m {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func MaxRes(mp [][]int, k int) int {
+	max := mp[0][0]
+	for i := 1; i < k ; i++ {
+		if mp[i][0] > max {
+			max = mp[i][0]
+		}
+	}
+	return max
+}
