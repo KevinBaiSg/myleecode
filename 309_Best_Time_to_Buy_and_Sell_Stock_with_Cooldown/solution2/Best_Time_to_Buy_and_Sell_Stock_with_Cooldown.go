593f136ae212c49bc8f6583b168b2f5165bf0dd5
Kevin Bai <kevin.bai.sin@gmail.com>
2019-05-30 11:56:38 +0800

    使用另一种DP状态定义方法解决问题309，性能更好

--- 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution2/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.go
+++ 309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/solution2/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.go
@@ -5,29 +5,21 @@ func maxProfit(prices []int) int {
 		return 0
 	}
 
-	buy := make([]int, len(prices), len(prices))
-	sell := make([]int, len(prices), len(prices))
-	cooldown := make([]int, len(prices), len(prices))
-
-	buy[0] = -prices[0]
-	sell[0] = 0
-	cooldown[0] = 0
+	var (
+		buy = -prices[0]
+		sell = 0
+		cooldown = 0
+	)
 
 	for i := 1; i < len(prices); i++ {
-		buy[i] = Max(buy[i-1], cooldown[i-1]-prices[i])
-		sell[i] = Max(buy[i-1]+prices[i], sell[i-1])
-		cooldown[i] = sell[i-1]
+		buy, sell, cooldown = Max(buy, cooldown-prices[i]), Max(buy+prices[i], sell), sell
 	}
-	return Max(sell[len(prices) - 1], cooldown[len(prices) - 1])
+	return Max(sell, cooldown)
 }
 
-func Max(m ...int) int {
-	if len(m) == 0 { return 0 }
-	max := m[0]
-	for _, v := range m {
-		if v > max {
-			max = v
-		}
+func Max(x, y int) int {
+	if x > y {
+		return x
 	}
-	return max
+	return y
 }
