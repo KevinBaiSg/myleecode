e2cd767cd44534b1ffc67852326827f1b94ea90e
Kevin Bai <kevin.bai.sin@gmail.com>
2019-07-24 16:29:19 +0800

    单独实现了queue 来实现问题0102

--- 0102_Binary_Tree_Level_Order_Traversal/solution1/main.go
+++ 0102_Binary_Tree_Level_Order_Traversal/solution1/main.go
@@ -21,32 +21,33 @@ func levelOrder(root *TreeNode) [][]int {
 		return [][]int{}
 	}
 	if root.Left == nil && root.Right == nil {
-	    return [][]int{
+		return [][]int{
 			{root.Val},
-	    }
+		}
 	}
 
-	queue := make([]*TreeNode, 0)
+	queue := Queue{}
+	queue.NewQueue()
 	levels := make([][]int, 0)
 
-	queue = append(queue, root)
+	queue.Enqueue(root)
 
-	for len(queue) != 0 {
-		n, level := len(queue), make([]int, 0)
+	for !queue.isEmpty() {
+		n, level := queue.Size(), make([]int, 0)
 
 		// 处理 level
 		for i := 0; i < n; i++ {
 			// 处理树
-			if queue[0].Left != nil {
-				queue = append(queue, queue[0].Left) // push()
+			if queue.Peek().Left != nil {
+				queue.Enqueue(queue.Peek().Left)
 			}
 
-			if queue[0].Right != nil {
-				queue = append(queue, queue[0].Right) // push()
+			if queue.Peek().Right != nil {
+				queue.Enqueue(queue.Peek().Right)
 			}
 
-			level = append(level, queue[0].Val)
-			queue = queue[1:] // pop()
+			level = append(level, queue.Peek().Val)
+			queue.Dequeue()
 		}
 
 		// 添加 level
@@ -54,4 +55,49 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 
 	return levels
+}
+
+type Queue struct {
+	items []*TreeNode
+}
+
+func (q *Queue) NewQueue() *Queue {
+	q.items = []*TreeNode{}
+	return q
+}
+
+func (q *Queue) isEmpty() bool {
+	if q.items == nil { return true }
+	return len(q.items) == 0
+}
+
+func (q *Queue) Size() int {
+	if q.items == nil { return 0 }
+	return len(q.items)
+}
+
+func (q *Queue) Peek() *TreeNode {
+	if q.items == nil { return nil }
+
+	if len(q.items) != 0 {
+		return q.items[0]
+	}
+
+	return nil
+}
+
+func (q *Queue) Dequeue() *TreeNode {
+	if q.items == nil { return nil }
+
+	if len(q.items) == 0 { return nil }
+
+	n := q.items[0]
+	q.items = q.items[1:]
+	return n
+}
+
+func (q *Queue) Enqueue(node *TreeNode) {
+	if q.items == nil { return }
+
+	q.items = append(q.items, node)
 }
