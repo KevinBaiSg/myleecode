2451a4c9d451d032cbb3697827750b83022b7095
Kevin Bai <kevin.bai.sin@gmail.com>
2019-07-25 09:04:26 +0800

    解决问题 0104

---
+++ 0104_Maximum_Depth_of_Binary_Tree/solution1/main.go
@@ -0,0 +1,54 @@
+package solution1
+
+import . "github.com/KevinBaiSg/myleecode/common"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func levelOrder(root *TreeNode) [][]int {
+	// 边界
+	if root == nil {
+		return [][]int{}
+	}
+	if root.Left == nil && root.Right == nil {
+		return [][]int{
+			{root.Val},
+		}
+	}
+
+	queue := Queue{}
+	queue.NewQueue()
+	levels := make([][]int, 0)
+
+	queue.Enqueue(root)
+
+	for !queue.IsEmpty() {
+		n, level := queue.Size(), make([]int, 0)
+
+		// 处理 level
+		for i := 0; i < n; i++ {
+			// 处理树
+			if queue.Peek().Left != nil {
+				queue.Enqueue(queue.Peek().Left)
+			}
+
+			if queue.Peek().Right != nil {
+				queue.Enqueue(queue.Peek().Right)
+			}
+
+			level = append(level, queue.Peek().Val)
+			queue.Dequeue()
+		}
+
+		// 添加 level
+		levels = append(levels, level)
+	}
+
+	return levels
+}
---
+++ 0104_Maximum_Depth_of_Binary_Tree/solution1/main_test.go
@@ -0,0 +1,11 @@
+package solution1
+
+import (
+	"testing"
+
+	. "github.com/KevinBaiSg/myleecode/common"
+)
+
+func TestIsScramble(t *testing.T)  {
+	levelOrder(MakeTree("[3,9,20,null,null,15,7]"))
+}
