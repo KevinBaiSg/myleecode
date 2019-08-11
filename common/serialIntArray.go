8fef75f1d694af1404249ee9b748112e82729887
Kevin Bai <kevin.bai.sin@gmail.com>
2019-08-11 17:24:23 +0800

    add SerialIntArray function

--- common/serialIntArray.go
+++ common/serialIntArray.go
@@ -1,1 +1,17 @@
 package common
+
+import "strconv"
+
+func SerialIntArray(array []int) (serial string) {
+	if len(array) == 0 { return "[]" }
+
+	for i, element := range array {
+		if i == 0 {
+			serial = "[" + strconv.Itoa(element)
+		} else {
+			serial = serial + ", " + strconv.Itoa(element)
+		}
+	}
+
+	return serial + "]"
+}
