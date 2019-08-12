d0deabda115461be833c6689e817991e1755feab
Kevin Bai <kevin.bai.sin@gmail.com>
2019-08-12 10:37:23 +0800

    add MergerSort

---
+++ common/sort/msort_test.go
@@ -0,0 +1,34 @@
+package sort
+
+import (
+	"testing"
+
+	. "github.com/KevinBaiSg/myleecode/common"
+)
+
+func TestMergeSort(t *testing.T) {
+	cases := []struct{
+		in		[]int
+		want 	[]int
+	}{
+		{[]int{}, []int{}},
+		{[]int{-10, 200, 3, 6, 8, 30}, []int{-10, 3, 6, 8, 30, 200}},
+	}
+	for _, c := range cases {
+		sort := MergeSort(c.in)
+		if len(c.want) != len(sort) {
+			t.Fatalf("sort error, want: %s; got: %s", SerialIntArray(c.want), SerialIntArray(sort))
+		}
+		for i := 0; i < len(sort); i++ {
+			if sort[i] != c.want[i] {
+				t.Fatalf("sort error, want: %s; got: %s", SerialIntArray(c.want), SerialIntArray(sort))
+			}
+		}
+	}
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		MergeSort([]int{-10, 200, 3, 6, 8, 30})
+	}
+}
