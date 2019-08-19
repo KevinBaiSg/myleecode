d4a44c2979be753335da37eb813583761c6c2127
Kevin Bai <kevin.bai.sin@gmail.com>
2019-08-19 22:07:30 +0800

    add 42. Trapping Rain Water

---
+++ 0042_Trapping_Rain_Water/solution1/main_test.go
@@ -0,0 +1,27 @@
+package solution1
+
+import (
+	"testing"
+)
+
+func TestIsScramble(t *testing.T)  {
+	cases := []struct{
+		in 		[]int
+		want	int
+	} {
+		{[]int{0,1,0,2,1,0,1,3,2,1,2,1}, 6},
+	}
+
+	for i, c := range cases {
+		ret := trap(c.in)
+		if ret != c.want {
+			t.Fatalf("index:%d failed; want %d; got %d", i, c.want, ret)
+		}
+	}
+}
+
+func BenchmarkIsScramble(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		trap([]int{0,1,0,2,1,0,1,3,2,1,2,1})
+	}
+}
